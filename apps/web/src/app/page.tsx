7887faed4d568b8b904638fa7d1bc69136a90348
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-05-13 21:27:17 +0200

    dockerize web and api

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 import { createUser } from "api-client/axios";
 
+// We need to prevent static generation, since the API is not available at build time
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const user = await createUser({
     email: "test@test.com",
     name: "test",
-  });
+  }).catch(() => ({ message: "Error :(" }));
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">

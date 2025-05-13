0a05961e1f4f8e25dc69fb2ddb54f05fd9dd21b3
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-05-13 16:38:25 +0200

    add test endpoint

--- apps/api/api/main.py
+++ apps/api/api/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
+from pydantic import BaseModel
 
-app = FastAPI()
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    if route.operation_id:
+        return route.operation_id
+
+    name_parts = route.name.split("_")
+    return name_parts[0] + "".join(part.capitalize() for part in name_parts[1:])
+
+
+app = FastAPI(
+    generate_unique_id_function=custom_generate_unique_id,
+)
 
 # --- CORS Configuration ---
 origins = [
@@ -18,12 +31,26 @@ app.add_middleware(
 )
 
 
+class User(BaseModel):
+    name: str
+    email: str
+
+
+class UserResponse(BaseModel):
+    message: str
+
+
 @app.get("/")
-async def read_root():
+async def root():
     # This endpoint is less relevant when running via Uvicorn directly on main:app
     return {"message": "Hello from FastAPI Backend (root of app object)!"}
 
 
 @app.get("/hello")
-async def read_api_hello():
+async def hello_world():
     return {"message": "API says: Hello World, from Python!"}
+
+
+@app.post("/users")
+async def create_user(user: User) -> UserResponse:
+    return UserResponse(message="User created successfully!")

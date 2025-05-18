1699ad3b726842ee28e65db3206c1786238de8e5
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-05-18 16:14:07 +0200

    refresh token if expired

--- apps/web/src/clients/api.ts
+++ apps/web/src/clients/api.ts
@@ -1,11 +1,32 @@
 import useAuthStore from "@/stores/auth.store";
 import { refreshToken } from "api-client";
 import { client } from "api-client/client";
+import { jwtDecode } from "jwt-decode";
 
 let isRefreshing = false;
 
 if (typeof window !== "undefined") {
-  // TODO: Refresh at request when near expiry instead of refreshing after the fact
+  client.interceptors.request.use(async (request) => {
+    const accessToken = request.headers.get("Authorization")?.split(" ")[1];
+
+    if (!accessToken) return request;
+
+    const decodedToken = jwtDecode(accessToken);
+
+    if (!decodedToken.exp || decodedToken.exp > Date.now() / 1000) {
+      return request;
+    }
+
+    const { data, error } = await refreshToken({});
+
+    if (data && !error) {
+      useAuthStore.getState().updateToken(data.access_token);
+      request.headers.set("Authorization", `Bearer ${data.access_token}`);
+    }
+
+    return request;
+  });
+
   client.interceptors.response.use(async (response) => {
     if (
       response.status === 401 &&

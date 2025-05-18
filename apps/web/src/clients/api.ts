951db30e0e9574d0905536107008347c24a9de5b
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-05-18 15:21:16 +0200

    prevent loop when refresh 401s

--- apps/web/src/clients/api.ts
+++ apps/web/src/clients/api.ts
@@ -2,16 +2,27 @@ import useAuthStore from "@/stores/auth.store";
 import { refreshToken } from "api-client";
 import { client } from "api-client/client";
 
+let isRefreshing = false;
+
 if (typeof window !== "undefined") {
   // TODO: Refresh at request when near expiry instead of refreshing after the fact
   client.interceptors.response.use(async (response) => {
-    if (response.status === 401 && useAuthStore.getState().token) {
-      const { data, error } = await refreshToken({});
+    if (
+      response.status === 401 &&
+      useAuthStore.getState().token &&
+      !isRefreshing
+    ) {
+      isRefreshing = true;
+      try {
+        const { data, error } = await refreshToken({});
 
-      if (data && !error) {
-        useAuthStore.getState().updateToken(data.access_token);
-      } else {
-        useAuthStore.getState().clearToken();
+        if (data && !error) {
+          useAuthStore.getState().updateToken(data.access_token);
+        } else {
+          useAuthStore.getState().clearToken();
+        }
+      } finally {
+        isRefreshing = false;
       }
     }
 

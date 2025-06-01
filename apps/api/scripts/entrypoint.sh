a119929209aa441820f55045ae3847f98b2e485e
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-06-02 00:41:20 +0200

    remove proxy header use

--- apps/api/scripts/entrypoint.sh
+++ apps/api/scripts/entrypoint.sh
@@ -3,6 +3,4 @@
 # Run migrations
 alembic upgrade head
 
-export FORWARDED_ALLOW_IPS='*'
-
-uvicorn api.main:app --host 0.0.0.0 --port 8000 --proxy-headers --no-server-header --no-date-header --workers $(nproc)
+uvicorn api.main:app --host 0.0.0.0 --port 8000 --no-server-header --no-date-header --workers $(nproc)
